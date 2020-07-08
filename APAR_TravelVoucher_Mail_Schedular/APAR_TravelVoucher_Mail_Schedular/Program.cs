3d26336cf27b68c63e2bce344c898f559e1602b0
vishal <vishal@ascenwork.com>
2020-07-08 18:39:10 +0530

    first

---
+++ APAR_TravelVoucher_Mail_Schedular/APAR_TravelVoucher_Mail_Schedular/Program.cs
@@ -0,0 +1,74 @@
+﻿using APAR_TravelVoucher_Mail_Schedular.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using APAR_EmployeeMaster_Schedular.Models;
+
+namespace APAR_TravelVoucher_Mail_Schedular
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string filename = "log\\Log.txt";
+            //CustomSharePointUtility.logFile = new StreamWriter(filename);
+            //CustomSharePointUtility.WriteLog("*********************************************");
+            //CustomSharePointUtility.WriteLog("Reminder Mail Starts: " + DateTime.Now.ToString());
+            //CustomSharePointUtility.WriteLog("*********************************************");
+            Console.WriteLine("*********************************************");
+            Console.WriteLine("Reminder Mail starts : " + DateTime.Now.ToString());
+            Console.WriteLine("*********************************************");
+            List<TravelVoucher> SPTravelVoucher = null;
+            try
+            {
+                var siteUrl = ConfigurationManager.AppSettings["SP_Address_Live"];
+                string TestingTravelHeaderList = ConfigurationManager.AppSettings["TestingTravelHeaderList"];
+                string EmailList = ConfigurationManager.AppSettings["EmailList"];
+                string DaysDifference = ConfigurationManager.AppSettings["DaysDifference"];
+                //string query = SQLUtility.ReadQuery("EmployeeMasterQuery.txt");
+                SPTravelVoucher = new List<TravelVoucher>();
+                //Task task_SPEmployeeMaster = Task.Run(() => SPTravelVoucher = CustomSharePointUtility.GetAll_TravelVoucherFromSharePoint(siteUrl, TestingTravelHeaderList));
+                SPTravelVoucher = CustomSharePointUtility.GetAll_TravelVoucherFromSharePoint(siteUrl, TestingTravelHeaderList, DaysDifference);
+                //List<TravelVoucher> empMasterFinal = new List<TravelVoucher>();
+                List<TravelVoucher> empMasterFinal = SPTravelVoucher;
+                if (empMasterFinal.Count > 0)
+                {
+                    //CustomSharePointUtility.WriteLog("Voucher data successfully.");
+                    //Console.WriteLine("Employee data synchronized successfully.");
+                    var success = CustomSharePointUtility.EmailData(empMasterFinal, siteUrl, EmailList);
+                    if (success)
+                    {
+                        CustomSharePointUtility.WriteLog("Reminder Mail Sent Successfully.");
+                        Console.WriteLine("Reminder Mail Sent Successfully.");
+                    }
+                }
+                else
+                {
+                    CustomSharePointUtility.WriteLog("No Pending Records.");
+                    Console.WriteLine("No Pending Records.");
+                }
+            }
+            catch (Exception ex)
+            {
+                CustomSharePointUtility.WriteLog("Error in scheduler : " + ex.StackTrace);
+                Console.WriteLine("Error in scheduler : " + ex.StackTrace);
+            }
+            finally
+            {
+                //CustomSharePointUtility.WriteLog("*********************************************");
+                //CustomSharePointUtility.WriteLog("Reminder Mail ends : " + DateTime.Now.ToString());
+                //CustomSharePointUtility.WriteLog("*********************************************");
+                Console.WriteLine("*********************************************");
+                Console.WriteLine("Reminder Mail ends : " + DateTime.Now.ToString());
+                Console.WriteLine("*********************************************");
+                CustomSharePointUtility.logFile.Close();
+                //Console.ReadKey();
+
+            }
+        }
+    }
+}
